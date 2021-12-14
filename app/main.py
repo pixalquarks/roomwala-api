910bbf599f8ff67ead9baf076e72d88392ad6666
pixalquarks <kashyaprakshat@gmail.com>
2021-12-14 23:06:12 +0530

    second attemp to rectify alembic database error

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from .routers import owner, auth, flats
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
