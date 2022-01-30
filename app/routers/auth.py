4d483c40396ff056ac17d0a5ee0c98bea1bd2056
pixalquarks <kashyaprakshat@gmail.com>
2022-01-30 21:31:46 +0530

    added verification route for owner verification

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -27,4 +27,17 @@ def owner_login(owner_credentials: OAuth2PasswordRequestForm = Depends(), db : S
     access_token = oauth2.create_access_token(data={"owner_id" : owner.id})
     
     return {"access_token" : access_token, "token_type" : "bearer"}
-    
+
+@router.get("/verification", status_code=status.HTTP_201_CREATED)
+def owner_verification(db : Session = Depends(get_db), token : str = ""):
+    user = oauth2.get_current_user(token)
+
+    owner_query = db.query(models.Owner).filter(models.Owner.id == user.id)
+
+    if owner_query.first().activated:
+        return {"message" : "Account already activated"}
+
+    owner_query.update({models.Owner.activated: 1})
+    db.commit()
+
+    return owner_query.first()
