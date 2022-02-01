32410265e5aec5bea3ee932667fc6733e787c8b6
pixalquarks <kashyaprakshat@gmail.com>
2022-02-01 22:37:23 +0530

    raise HTTPException when trying to log an account that is not activated, raise HTTPException when trying to activate an account that is already activated, defined response_model for verification route

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -24,18 +24,21 @@ def owner_login(owner_credentials: OAuth2PasswordRequestForm = Depends(), db : S
         print("invalid pass")
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"invalid login credentials")
     
+    if not owner.activated:
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=f"account is not activated, cannot login")
+    
     access_token = oauth2.create_access_token(data={"owner_id" : owner.id})
     
     return {"access_token" : access_token, "token_type" : "bearer"}
 
-@router.get("/verification", status_code=status.HTTP_201_CREATED)
+@router.get("/verification", status_code=status.HTTP_201_CREATED, response_model=schemas.OwnerOut)
 def owner_verification(db : Session = Depends(get_db), token : str = ""):
     user = oauth2.get_current_user(token)
 
     owner_query = db.query(models.Owner).filter(models.Owner.id == user.id)
 
     if owner_query.first().activated:
-        return {"message" : "Account already activated"}
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"account has already been activated")
 
     owner_query.update({models.Owner.activated: 1})
     db.commit()
